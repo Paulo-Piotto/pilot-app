077749658eeda91e17de29da7ebcb560f9426f71
oErco <erickrsilva@hotmail.com>
2022-12-18 01:22:19 -0300

    Animation added to cards

--- src/components/generics/card.js
+++ src/components/generics/card.js
@@ -1,26 +1,31 @@
 import { CardStyle } from "../../styles/cardStyles";
+import { Grow } from "@material-ui/core";
 
-export default function Card({contrast, subtitle, title, iconName, number, action, money}){
+export default function Card({contrast, subtitle, title, iconName, number, action, money, delay}){
+
+    const delayValue = delay ? delay : '0ms'
+
     return(
-        <CardStyle contrast={contrast} onClick={action} >
-            {!contrast ? (
-                <>
-                <section>
-                    <p>{subtitle}</p>
-                    <h2>{title}</h2>
-                </section>
-                <div>
-                    <ion-icon name={iconName}></ion-icon>
-                </div>
-                </>
-                
-            ) : (
-                <section>
-                    <h4>{subtitle}</h4>
-                    <h3>{money ? 'R$' : ''} {number}</h3>
-                </section>
-            )}
-            
-        </CardStyle>
+        <Grow in={true} timeout={{ enter: 500, exit: 500 }} style={{ transitionDelay: delayValue }}>
+            <CardStyle contrast={contrast} onClick={action} >
+                {!contrast ? (
+                    <>
+                    <section>
+                        <p>{subtitle}</p>
+                        <h2>{title}</h2>
+                    </section>
+                    <div>
+                        <ion-icon name={iconName}></ion-icon>
+                    </div>
+                    </>
+                    
+                ) : (
+                    <section>
+                        <h4>{subtitle}</h4>
+                        <h3>{money ? 'R$' : ''} {number}</h3>
+                    </section>
+                )}
+            </CardStyle>
+        </Grow>
     );
 }
