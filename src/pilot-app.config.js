0b1da5933b4c6830339c836c626becdc8eecf90e
Paulo <paulo_piotto@hotmail.com>
2023-01-25 13:50:35 -0300

    create environment variables

--- src/pilot-app.config.js
+++ src/pilot-app.config.js
@@ -1,13 +1,14 @@
 const Config = {
-    serverBaseURL: 'http://localhost:5000',
-    //serverBaseURL: 'http://3.82.151.240:80/',
+    serverBaseURL: process.env.REACT_APP_SERVER_URL,
     rolesLevel: {// Represents the hierarchy of roles. The grater number is the greater role, and so on
         new: 0,
         basic: 1,
         admin: 2,
         root: 3,
     },
-    appPrivateKey: "elespassaraoeupassarinho"
+    appPrivateKey: process.env.REACT_APP_PRIVATE_KEY
 }
+
+console.log(process.env.REACT_APP_SERVER_URL);
 
 export default Config;
