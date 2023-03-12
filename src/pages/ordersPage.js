4f9ed9ef600ae0668aab93e64ed937548ecbb198
Paulo <paulo_piotto@hotmail.com>
2023-03-12 19:43:39 -0300

    add orders pdf generation

--- src/pages/ordersPage.js
+++ src/pages/ordersPage.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import { CardsContainer } from "../styles/cardStyles";
 import Card from "../components/generics/card";
-import CircularProgress from '@mui/material/CircularProgress';
-import { TableContainer, TableHeader, HeaderContainer} from "../styles/tableStyles";
+import { CircularProgress } from "@material-ui/core";
+import PrintIcon from "@mui/icons-material/Print";
+import {
+  TableContainer,
+  TableHeader,
+  HeaderContainer,
+} from "../styles/tableStyles";
 import OrderItem from "../components/orders/orderItem";
 import { OrdersService } from "../services/api.services";
 import { sumTotal } from "../services/utils/sumTotal";
@@ -11,95 +16,139 @@ import AddOrderDialog from "../components/orders/addOrderDialog";
 import GenericSnackbar from "../components/generics/genericSnackbar";
 import intToMoney from "../services/utils/intToMoney";
 import { Container } from "../components/generics/inProgress";
-import { Clear, Loading } from "../styles/generalStyles";
+import { Clear, Loading, PrintButton } from "../styles/generalStyles";
+import pdfGenerator from "../components/pdf/pdfGenerator";
 
-export default function OrdersPage(){    
-    const [orders, setOrders] = useState([]);
-    const [total, setTotal] = useState('0,00')
-    const [openSearch, setOpenSearch] = useState(false);
-    const [openAdd, setOpenAdd] = useState(false);
-    const [snackbar, setSnackbar] = useState(false);
-    const [snackbarType, setSnackbarType] = useState('success');
-    const [snackbarMessage, setSnackbarMessage] = useState('Item deletado com sucesso')
-    const [loading, setLoading] = useState(true);
+export default function OrdersPage() {
+  const [orders, setOrders] = useState([]);
+  const [total, setTotal] = useState("0,00");
+  const [openSearch, setOpenSearch] = useState(false);
+  const [openAdd, setOpenAdd] = useState(false);
+  const [snackbar, setSnackbar] = useState(false);
+  const [snackbarType, setSnackbarType] = useState("success");
+  const [snackbarMessage, setSnackbarMessage] = useState(
+    "Item deletado com sucesso"
+  );
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        OrdersService.getAllOrders()
-            .then((resp) => {
-                setOrders(resp.data)
-                setTotal(intToMoney(sumTotal(resp.data)));
-                setLoading(false)
-            })
-            .catch(() => {
-                setSnackbarType('error');
-                setSnackbarMessage('Algo deu errado ao recuperar os itens')
-                setSnackbar(true);
-                setLoading(false);
-            })
-    }, [])
+  useEffect(() => {
+    OrdersService.getAllOrders()
+      .then((resp) => {
+        setOrders(resp.data);
+        setTotal(intToMoney(sumTotal(resp.data)));
+        setLoading(false);
+      })
+      .catch(() => {
+        setSnackbarType("error");
+        setSnackbarMessage("Algo deu errado ao recuperar os itens");
+        setSnackbar(true);
+        setLoading(false);
+      });
+  }, []);
 
-    function handleCloseDialog(){
-        setOpenSearch(false);
-        setOpenAdd(false);
-    }
+  function handleCloseDialog() {
+    setOpenSearch(false);
+    setOpenAdd(false);
+  }
 
-    function clearFilters(){
-        setLoading(true);
-        OrdersService.getAllOrders()
-        .then((resp) => {
-            setOrders(resp.data)
-            setTotal(intToMoney(sumTotal(resp.data)));
-            setLoading(false)
-        })
-        .catch(() => {
-            setSnackbarType('error');
-            setSnackbarMessage('Algo deu errado ao recuperar os itens')
-            setSnackbar(true);
-            setLoading(false);
-        })
-    }
+  function clearFilters() {
+    setLoading(true);
+    OrdersService.getAllOrders()
+      .then((resp) => {
+        setOrders(resp.data);
+        setTotal(intToMoney(sumTotal(resp.data)));
+        setLoading(false);
+      })
+      .catch(() => {
+        setSnackbarType("error");
+        setSnackbarMessage("Algo deu errado ao recuperar os itens");
+        setSnackbar(true);
+        setLoading(false);
+      });
+  }
 
-
-    return (
+  return (
+    <>
+      <Clear onClick={clearFilters}>Limpar filtros</Clear>
+      <CardsContainer>
+        <Card
+          contrast={false}
+          subtitle="Novo"
+          title="Pedido"
+          iconName="add-circle-outline"
+          action={() => setOpenAdd(true)}
+        />
+        <Card
+          contrast={false}
+          subtitle="Configurações de"
+          title="Busca"
+          iconName="search-outline"
+          action={() => setOpenSearch(true)}
+        />
+        <Card contrast={true} subtitle="Total" number={total} money={true} />
+      </CardsContainer>
+      <AddOrderDialog
+        openDialog={openAdd}
+        handleCloseDialog={handleCloseDialog}
+        setOrders={setOrders}
+        setTotal={setTotal}
+        setLoading={setLoading}
+      />
+      <SearchOrdersDialog
+        openDialog={openSearch}
+        handleCloseDialog={handleCloseDialog}
+        setOrders={setOrders}
+        setTotal={setTotal}
+        setLoading={setLoading}
+      />
+      <GenericSnackbar
+        snackbar={snackbar}
+        setSnackbar={setSnackbar}
+        type={snackbarType}
+        message={snackbarMessage}
+      />
+      {loading ? (
+        <Loading>
+          {" "}
+          <CircularProgress />{" "}
+        </Loading>
+      ) : (
         <>
-        <Clear onClick={clearFilters} >Limpar filtros</Clear>
-        <CardsContainer>
-            <Card contrast={false} subtitle='Novo' title='Pedido' iconName='add-circle-outline' action={() => setOpenAdd(true)} />
-            <Card contrast={false} subtitle='Configurações de' title='Busca' iconName='search-outline' action={() => setOpenSearch(true)}/>
-            <Card contrast={true} subtitle='Total' number={total} money={true}/>
-        </CardsContainer>
-        <AddOrderDialog openDialog={openAdd} handleCloseDialog={handleCloseDialog} setOrders={setOrders} setTotal={setTotal} setLoading={setLoading} />
-        <SearchOrdersDialog openDialog={openSearch} handleCloseDialog={handleCloseDialog} setOrders={setOrders} setTotal={setTotal} setLoading={setLoading} />
-        <GenericSnackbar snackbar={snackbar} setSnackbar={setSnackbar} type={snackbarType} message={snackbarMessage} />
-        {loading ? <Loading> <CircularProgress /> </Loading> :
-        (
+          {orders[0] ? (
             <>
-            {orders[0] ? (
-            <>
-            <HeaderContainer>
+              <HeaderContainer>
                 <TableHeader>
-                    <p>Pedido</p>
-                    <p>Obra</p>
-                    <p>Loja</p>
-                    <p>Valor</p>
-                    <p>Data</p>
-                    <p></p>
+                  <p>Pedido</p>
+                  <p>Obra</p>
+                  <p>Loja</p>
+                  <p>Valor</p>
+                  <p>Data</p>
+                  <p></p>
+                  <PrintButton onClick={() => pdfGenerator(orders)}>
+                    <PrintIcon sx={{ color: "#EAEAEA" }} />
+                  </PrintButton>
                 </TableHeader>
-            </HeaderContainer>
-                <TableContainer>
-                {orders.map((order, index) => 
-                    <OrderItem key={index} rowData={order} setTotal={setTotal} setOrders={setOrders} setLoading={setLoading} setSnackbar={setSnackbar} setSnackbarType={setSnackbarType} setSnackbarMessage={setSnackbarMessage} />
-                )}
-            </TableContainer>
+              </HeaderContainer>
+              <TableContainer>
+                {orders.map((order, index) => (
+                  <OrderItem
+                    key={index}
+                    rowData={order}
+                    setTotal={setTotal}
+                    setOrders={setOrders}
+                    setLoading={setLoading}
+                    setSnackbar={setSnackbar}
+                    setSnackbarType={setSnackbarType}
+                    setSnackbarMessage={setSnackbarMessage}
+                  />
+                ))}
+              </TableContainer>
             </>
-            ) : 
-            <Container>
-                Nenhum item encontrado...
-            </Container>
-            }
-            </>
-        )
-        }
+          ) : (
+            <Container>Nenhum item encontrado...</Container>
+          )}
         </>
-    );
+      )}
+    </>
+  );
 }
