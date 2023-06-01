059400c2f0d00943b89703e9b9e344fd5d25f155
Paulo <paulo_piotto@hotmail.com>
2023-06-01 16:36:00 -0300

    implement fixed hour dates

--- src/components/punchCardManager/WorkDayDialog.js
+++ src/components/punchCardManager/WorkDayDialog.js
@@ -1,146 +1,189 @@
-import { Button, Dialog, DialogActions, DialogTitle, DialogContent } from '@mui/material';
-import { PunchCardService } from '../../services/api.services';
-import dayjs from 'dayjs';
-import { getWeekDayNameBasedOnInt } from './helpers';
-import { DialogContentText } from '@material-ui/core';
-import AuthContext from '../context/AuthContext';
-import PunchCardContext from '../context/PunchCardContext';
-import { useContext, useEffect, useState } from 'react';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import LogoLoadingSpinner from '../generics/logoLoadingSpinner';
-import { WorkDayDialogLoadingContainer } from "./styles"
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+} from "@mui/material";
+import { PunchCardService } from "../../services/api.services";
+import dayjs from "dayjs";
+import { getWeekDayNameBasedOnInt } from "./helpers";
+import { DialogContentText } from "@material-ui/core";
+import AuthContext from "../context/AuthContext";
+import PunchCardContext from "../context/PunchCardContext";
+import { useContext, useEffect, useState } from "react";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import LogoLoadingSpinner from "../generics/logoLoadingSpinner";
+import { WorkDayDialogLoadingContainer } from "./styles";
+import { averageDateHour } from "../../services/utils/dateServices";
 
-export default function WorkDayDialog({ openDialog, closeDialog, initialWorkDayData, employeeData }) {
-    const { userData } = useContext(AuthContext)
-    const { callSnackBar, refreshPunchCardData, clientOptions } = useContext(PunchCardContext)
-    const [ isLoading, setIsLoading ] = useState(false)
-    const [ mode, setMode ] = useState("preview")
-    const [ workDayData, setWorkDayData ] = useState({
-        ...initialWorkDayData,
-        isPresence: initialWorkDayData.id,
-        parsedDate: dayjs(initialWorkDayData.date).format("DD/MM/YYYY"),
-        weekDay: getWeekDayNameBasedOnInt(dayjs(initialWorkDayData.date).day())
-    })
-    const [ clients, setClients ] = useState({
-        options: clientOptions,
-        selectedId: 0
-    })
+export default function WorkDayDialog({
+  openDialog,
+  closeDialog,
+  initialWorkDayData,
+  employeeData,
+}) {
+  const { userData } = useContext(AuthContext);
+  const { callSnackBar, refreshPunchCardData, clientOptions } =
+    useContext(PunchCardContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [mode, setMode] = useState("preview");
+  const [workDayData, setWorkDayData] = useState({
+    ...initialWorkDayData,
+    isPresence: initialWorkDayData.id,
+    parsedDate: dayjs(initialWorkDayData.date).format("DD/MM/YYYY"),
+    weekDay: getWeekDayNameBasedOnInt(dayjs(initialWorkDayData.date).day()),
+  });
+  const [clients, setClients] = useState({
+    options: clientOptions,
+    selectedId: 0,
+  });
 
-    useEffect(() => {
-        setWorkDayData(prev => ({
-            ...prev,
-            ...initialWorkDayData,
-            isPresence: initialWorkDayData.id,
-            parsedDate: dayjs(initialWorkDayData.date).format("DD/MM/YYYY"),
-            weekDay: getWeekDayNameBasedOnInt(dayjs(initialWorkDayData.date).day())
-        }))
-    }, [initialWorkDayData])
+  useEffect(() => {
+    setWorkDayData((prev) => ({
+      ...prev,
+      ...initialWorkDayData,
+      isPresence: initialWorkDayData.id,
+      parsedDate: dayjs(initialWorkDayData.date).format("DD/MM/YYYY"),
+      weekDay: getWeekDayNameBasedOnInt(dayjs(initialWorkDayData.date).day()),
+    }));
+  }, [initialWorkDayData]);
 
-    useEffect(() => { 
-        setMode(prev => {
-            if(prev !== "preview") return "preview"
-            else return prev
-        })
-    }, [openDialog])
+  useEffect(() => {
+    setMode((prev) => {
+      if (prev !== "preview") return "preview";
+      else return prev;
+    });
+  }, [openDialog]);
 
-    async function deletePunch() { await PunchCardService.delete(workDayData.id, userData.token) }
+  async function deletePunch() {
+    await PunchCardService.delete(workDayData.id, userData.token);
+  }
 
-    async function createPunch() {
-        await PunchCardService.create({
-            clientId: clients.selectedId,
-            employeeId: employeeData.id,
-            date: workDayData.date
-        }, userData.token)
+  async function createPunch() {
+    await PunchCardService.create(
+      {
+        clientId: clients.selectedId,
+        employeeId: employeeData.id,
+        date: averageDateHour(workDayData.date),
+      },
+      userData.token
+    );
+  }
+
+  async function editHandler() {
+    setIsLoading(true);
+
+    if (!workDayData.isPresence && !clients.selectedId) {
+      callSnackBar({ message: "Atribua uma obra à presença", type: "warning" });
+      setIsLoading(false);
+      return;
     }
 
-    async function editHandler() {
-        setIsLoading(true)
+    try {
+      if (workDayData.isPresence) await deletePunch();
+      else await createPunch();
 
-        if(!workDayData.isPresence && !clients.selectedId) {
-            callSnackBar({  message: "Atribua uma obra à presença", type: "warning" })
-            setIsLoading(false)
-            return
-        }
-
-        try {
-            if(workDayData.isPresence) await deletePunch()
-            else await createPunch();
-
-            refreshPunchCardData()
-            callSnackBar({ message: "Alterações Salvas!", type: "success" })
-        } catch (error) {
-            console.error("Error while creating new punch in punchcard")
-            console.error(error)
-            callSnackBar({  message: "Falha ao editar presença", type: "error" })
-        }
-
-        setIsLoading(false)
-        handleClose()
+      refreshPunchCardData();
+      callSnackBar({ message: "Alterações Salvas!", type: "success" });
+    } catch (error) {
+      console.error("Error while creating new punch in punchcard");
+      console.error(error);
+      callSnackBar({ message: "Falha ao editar presença", type: "error" });
     }
 
-    function handleClose() {
-        setClients(prev => ({ ...prev, selectedId: 0 }))
-        setWorkDayData(prev => ({ ...prev, clients: undefined }))
-        closeDialog()
-    }
+    setIsLoading(false);
+    handleClose();
+  }
 
-    return (
-        <Dialog open={openDialog} onClose={handleClose} maxWidth='sm' fullWidth={true}>
-            <DialogTitle>Cuidado!</DialogTitle>
-            <p>{workDayData.date}</p>
-            <DialogContent>
-                <DialogContentText>{`Você está prestes a editar a presença referente ao dia ${workDayData.parsedDate}, ${workDayData.weekDay}`}</DialogContentText>
-            </DialogContent>
+  function handleClose() {
+    setClients((prev) => ({ ...prev, selectedId: 0 }));
+    setWorkDayData((prev) => ({ ...prev, clients: undefined }));
+    closeDialog();
+  }
 
-            <DialogContent>
-                <DialogContentText>{`Funcionário: ${employeeData.name}`}</DialogContentText>
-                <DialogContentText>
-                    {`${workDayData.weekDay}, ${workDayData.parsedDate}  `}
-                </DialogContentText>
+  return (
+    <Dialog
+      open={openDialog}
+      onClose={handleClose}
+      maxWidth="sm"
+      fullWidth={true}
+    >
+      <DialogTitle>Cuidado!</DialogTitle>
+      <p>{workDayData.date}</p>
+      <DialogContent>
+        <DialogContentText>{`Você está prestes a editar a presença referente ao dia ${workDayData.parsedDate}, ${workDayData.weekDay}`}</DialogContentText>
+      </DialogContent>
 
-                <DialogContentText>
-                   { `Status atual:   `}
-                    <Button variant="text">{(workDayData.isPresence && mode==="preview") || (!workDayData.isPresence && mode==="edit") ? "PRESENÇA" : "FALTA"}</Button>
-                </DialogContentText>
+      <DialogContent>
+        <DialogContentText>{`Funcionário: ${employeeData.name}`}</DialogContentText>
+        <DialogContentText>
+          {`${workDayData.weekDay}, ${workDayData.parsedDate}  `}
+        </DialogContentText>
 
-                <FormControl fullWidth>
-                    <InputLabel id="client_options">Obra</InputLabel>
-                    <Select
-                        id="outlined-select-client"
-                        labelId="client_options"
-                        disabled={mode === "preview" || !!workDayData.isPresence}
-                        value={mode === "preview" ? clients.options.find(client => client.id === workDayData.clients?.id)?.id ?? "" : clients.selectedId}
-                        label="Obra"
-                        error={!clients.selectedId}
-                        onChange={e => setClients(prev => ({...prev, selectedId: e.target.value}))}
-                    >{
-                        clients.options.map(client => <MenuItem key={client.id} value={client.id}>{client.name}</MenuItem>)
-                    }</Select>
-                </FormControl>
-            </DialogContent>
+        <DialogContentText>
+          {`Status atual:   `}
+          <Button variant="text">
+            {(workDayData.isPresence && mode === "preview") ||
+            (!workDayData.isPresence && mode === "edit")
+              ? "PRESENÇA"
+              : "FALTA"}
+          </Button>
+        </DialogContentText>
 
-            <DialogActions>
-                {
-                    isLoading
-                    ? <WorkDayDialogLoadingContainer><LogoLoadingSpinner width="60px" height="60px" /></WorkDayDialogLoadingContainer>
-                    : <>
-                        <Button onClick={handleClose}>{mode === "edit" ? "Cancelar" : "Fechar"}</Button>
-                        <Button variant='contained' 
-                                color='warning' 
-                                onClick={ mode === "preview" ? () => setMode("edit") : editHandler }
-                        >
+        <FormControl fullWidth>
+          <InputLabel id="client_options">Obra</InputLabel>
+          <Select
+            id="outlined-select-client"
+            labelId="client_options"
+            disabled={mode === "preview" || !!workDayData.isPresence}
+            value={
+              mode === "preview"
+                ? clients.options.find(
+                    (client) => client.id === workDayData.clients?.id
+                  )?.id ?? ""
+                : clients.selectedId
+            }
+            label="Obra"
+            error={!clients.selectedId}
+            onChange={(e) =>
+              setClients((prev) => ({ ...prev, selectedId: e.target.value }))
+            }
+          >
+            {clients.options.map((client) => (
+              <MenuItem key={client.id} value={client.id}>
+                {client.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </DialogContent>
 
-                            { mode === "preview"
-                                ? `Mudar Para ${workDayData.isPresence ? "FALTA" : "PRESENÇA"}`
-                                : "Salvar"
-                            }
-                        </Button>
-                    </>
-                }
-            </DialogActions>
-        </Dialog>
-    )
+      <DialogActions>
+        {isLoading ? (
+          <WorkDayDialogLoadingContainer>
+            <LogoLoadingSpinner width="60px" height="60px" />
+          </WorkDayDialogLoadingContainer>
+        ) : (
+          <>
+            <Button onClick={handleClose}>
+              {mode === "edit" ? "Cancelar" : "Fechar"}
+            </Button>
+            <Button
+              variant="contained"
+              color="warning"
+              onClick={mode === "preview" ? () => setMode("edit") : editHandler}
+            >
+              {mode === "preview"
+                ? `Mudar Para ${workDayData.isPresence ? "FALTA" : "PRESENÇA"}`
+                : "Salvar"}
+            </Button>
+          </>
+        )}
+      </DialogActions>
+    </Dialog>
+  );
 }
--- src/services/utils/dateServices.js
+++ src/services/utils/dateServices.js
@@ -1,18 +1,70 @@
-function floorDateHour(date){
-    const stringDate = date.toISOString();
-    const arrayDate = stringDate.split('');
-    arrayDate.splice(11, 12, '0','0',':','0','0',':','0','0','.','0','0','0')
-    return arrayDate.join('')
+function floorDateHour(date) {
+  const stringDate = date.toISOString();
+  const arrayDate = stringDate.split("");
+  arrayDate.splice(
+    11,
+    12,
+    "0",
+    "0",
+    ":",
+    "0",
+    "0",
+    ":",
+    "0",
+    "0",
+    ".",
+    "0",
+    "0",
+    "0"
+  );
+  return arrayDate.join("");
 }
 
-function ceilDateHour(date){
-    const stringDate = date.toISOString();
-    const arrayDate = stringDate.split('');
-    arrayDate.splice(11, 12, '2','3',':','5','9',':','5','9','.','9','9','9')
-    return arrayDate.join('')
+function ceilDateHour(date) {
+  const stringDate = date.toISOString();
+  const arrayDate = stringDate.split("");
+  arrayDate.splice(
+    11,
+    12,
+    "2",
+    "3",
+    ":",
+    "5",
+    "9",
+    ":",
+    "5",
+    "9",
+    ".",
+    "9",
+    "9",
+    "9"
+  );
+  return arrayDate.join("");
 }
 
-export{
-    floorDateHour,
-    ceilDateHour,
+function averageDateHour(date) {
+  let stringDate;
+  typeof date !== "string"
+    ? (stringDate = date.toISOString())
+    : (stringDate = date);
+  const arrayDate = stringDate.split("");
+  arrayDate.splice(
+    11,
+    12,
+    "1",
+    "2",
+    ":",
+    "0",
+    "0",
+    ":",
+    "0",
+    "0",
+    ".",
+    "0",
+    "0",
+    "0"
+  );
+  return arrayDate.join("");
 }
+
+export { floorDateHour, ceilDateHour, averageDateHour };
